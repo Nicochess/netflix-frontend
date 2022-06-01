1d3a55b8dbc26b0bc597103a2ee950743a3287c1
Nicolas Magno <nicolasmagno.100@gmail.com>
2022-06-01 18:18:02 -0300

    feat: second class

--- src/screens/login/login.styled.tsx
+++ src/screens/login/login.styled.tsx
@@ -5,3 +5,27 @@ export const Wrapper = styled(Grid)`
     min-height: 100vh;
     background-color: #1A1A1A;
 `
+
+export const Input = styled.input`
+    width: 100%;
+    padding: 9px;
+    margin: 0 0 22px;
+    border-radius: 5px;
+    border: 1px solid #E0E0E0;
+`
+
+export const Button = styled.button`
+    background-color: #e31a13;
+    width: 100%;
+    color: #FFF;
+    padding: 9px;
+    margin: 0 0 22px;
+    border-radius: 5px;
+    border: 0;
+    cursor: pointer;
+`
+
+export const Error = styled.p`
+    color: #e31a13;
+    font-weight: 800;
+`
--- src/screens/login/login.tsx
+++ src/screens/login/login.tsx
@@ -1,14 +1,62 @@
-import { useCallback } from "react";
-import { Wrapper } from "./login.styled";
+import { Grid } from "@mui/material";
+import { useCallback, useState, useEffect } from "react";
+import { Wrapper, Input, Button, Error } from "./login.styled";
+import * as yup from 'yup';
 
 export default function Login () {
-    const handleChanges = useCallback(( event: any ) => {
-        console.log(event.target.value)
+    const [error, setError] = useState([])
+    const [data, setData] = useState({
+        email: "",
+        password: ""
+    })
+    const [loading, setLoading] = useState(false)
+
+    const handleChanges = useCallback(( { target }: any ) => {
+        console.log(target.value)
+
+        setData(prevData => ({
+            ...prevData,
+            [target.name]: target.value
+        }))
+    }, [setData])
+
+    const handleSend = useCallback( async () => {
+        try {
+            const schema = yup.object().shape({
+                email: yup.string().required().email(),
+                password: yup.string().required(),
+            })
+
+            await schema.validate(data)
+        } catch (e: any) {
+            setError(e.errors[0])
+        }
+    }, [data])
+
+    useEffect(() => {
+        yup.object().shape({
+            
+        })
     }, [])
 
     return (
         <Wrapper container justifyContent="center" alignContent="center">
-            <input type="email" name="email" onChange={handleChanges} />
+            <Grid item={true} xs={2} container justifyContent="center" alignContent="center">
+                <Input 
+                    type="email" 
+                    name="email" 
+                    placeholder="Email" 
+                    onChange={handleChanges} 
+                />
+                <Input 
+                    type="password" 
+                    name="password" 
+                    placeholder="Password" 
+                    onChange={handleChanges} 
+                />
+                <Button onClick={handleSend}>Entrar</Button>
+                <Error>{error}</Error>
+            </Grid>
         </Wrapper>
     )
 }
